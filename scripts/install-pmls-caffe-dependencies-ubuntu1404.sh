4c1ec62aba48f1047dac8bbe2ab232b9055ab47c
Raajay Viswanathan <raajay.v@gmail.com>
2017-09-02 10:40:14 -0500

    update dependencies

--- scripts/install-pmls-caffe-dependencies-ubuntu1404.sh
+++ scripts/install-pmls-caffe-dependencies-ubuntu1404.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# thirdparty deps
 sudo apt-get install -y g++ make python-dev libxml2-dev libxslt-dev git zlibc zlib1g zlib1g-dev libbz2-1.0 libbz2-dev
+
+# caffe deps
+sudo apt-get install -y libatlas-base-dev libprotobuf-dev libsnappy-dev libopencv-dev libhdf5-serial-dev
+sudo apt-get install -y libnuma-dev uuid-dev
