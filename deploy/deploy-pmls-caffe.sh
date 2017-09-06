e0b41f14eaa332a1ca439ad769f30876ca1818b2
Raajay Viswanathan <raajay.v@gmail.com>
2017-09-06 16:29:23 -0500

    deploy third party libraries too

--- deploy/deploy-pmls-caffe.sh
+++ deploy/deploy-pmls-caffe.sh
@@ -24,12 +24,13 @@ CAFFE_ROOT="${PMLS_CAFFE_ROOT_DIR:-${SCRIPT_ROOT}}"
 BUILD_DIR=$CAFFE_ROOT/.build_release/
 TAR_FILE_NAME="pmls-caffe.tar.gz"
 TAR_FILE=$SCRIPT_DIR/$TAR_FILE_NAME
+THIRD_PARTY_LIBS="${CAFFE_ROOT}/third_party/lib"
 
 rm -f $TAR_FILE
 tar --absolute-names -czvf $TAR_FILE \
     --exclude '*.o' \
     --exclude '*.o.warnings.txt' \
-    $BUILD_DIR ${CAFFE_ROOT}/build
+    $BUILD_DIR ${CAFFE_ROOT}/build ${THIRD_PARTY_LIBS}
 
 pdsh -R ssh -w \^${HOST_FILE} "rm -f ~/${TAR_FILE_NAME}"
 pdcp -R ssh -w \^${HOST_FILE} $TAR_FILE ~/${TAR_FILE_NAME}
