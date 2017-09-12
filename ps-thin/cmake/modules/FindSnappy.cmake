c64d0037976432bf91bf8108a599ee79ca97d6bc
Raajay Viswanathan <raajay.v@gmail.com>
2017-09-12 00:39:20 -0500

    fix snappy include

--- ps-thin/cmake/modules/FindSnappy.cmake
+++ ps-thin/cmake/modules/FindSnappy.cmake
@@ -31,23 +31,23 @@
 # Variables defined by this module:
 #
 #  SNAPPY_FOUND              System has Snappy libs/headers
-#  SNAPPY_LIBRARIES          The Snappy libraries
-#  SNAPPY_INCLUDE_DIR        The location of Snappy headers
+#  Snappy_LIBRARIES          The Snappy libraries
+#  Snappy_INCLUDE_DIR        The location of Snappy headers
 
-find_path(SNAPPY_INCLUDE_DIR
+find_path(Snappy_INCLUDE_DIR
     NAMES snappy.h
     HINTS ${SNAPPY_ROOT_DIR}/include)
 
-find_library(SNAPPY_LIBRARIES
+find_library(Snappy_LIBRARIES
     NAMES snappy
     HINTS ${SNAPPY_ROOT_DIR}/lib)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Snappy DEFAULT_MSG
-    SNAPPY_LIBRARIES
-    SNAPPY_INCLUDE_DIR)
+    Snappy_LIBRARIES
+    Snappy_INCLUDE_DIR)
 
 mark_as_advanced(
     SNAPPY_ROOT_DIR
-    SNAPPY_LIBRARIES
-    SNAPPY_INCLUDE_DIR)
+    Snappy_LIBRARIES
+    Snappy_INCLUDE_DIR)
