cbf772aa1e648d1a81a067eee6ed593906de01de
Raajay Viswanathan <raajay.v@gmail.com>
2017-09-16 11:02:23 -0600

    does not compile with caffe: modifying the interface of table for ps-thin

--- ps-thin/src/petuum_ps/client/abstract_client_table.hpp
+++ ps-thin/src/petuum_ps/client/abstract_client_table.hpp
@@ -24,7 +24,7 @@ public:
 
   virtual void FlushThreadCache() = 0;
 
-  virtual ClientRow *Get(int32_t row_id, RowAccessor *row_accessor) = 0;
+  virtual ClientRow *Get(int32_t row_id, RowAccessor *row_accessor, int32_t clock) = 0;
 
   virtual void Inc(int32_t row_id,
                    int32_t column_id,
