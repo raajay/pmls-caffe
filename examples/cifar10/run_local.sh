f658f2f3e8b59ed09ab91578ea0407af412e6b3b
Raajay Viswanathan <raajay.v@gmail.com>
2017-09-25 17:47:52 -0600

    update local run script

--- examples/cifar10/run_local.sh
+++ examples/cifar10/run_local.sh
@@ -13,14 +13,14 @@ app_dir="${PMLS_CAFFE_ROOT_DIR:-${script_root_dir}}"
 third_party_libs="${app_dir}/third_party/lib"
 caffe_libs="${PMLS_CAFFE_ROOT_DIR}/build/lib"
 
-progname=caffe_main
+progname=caffe_main.bin
 prog_path=${app_dir}/build/tools/${progname}
 
-#host_filename="${script_dir}/machinefiles"
-#host_file=$(readlink -f $host_filename)
-#
-#worker_filename="${script_dir}/workerfiles"
-#worker_file=$(readlink -f $worker_filename)
+host_filename="${script_dir}/localmachinefiles"
+host_file=$(readlink -f $host_filename)
+
+worker_filename="${script_dir}/localmachinefiles"
+worker_file=$(readlink -f $worker_filename)
 
 dataset=cifar10
 
@@ -50,8 +50,9 @@ ssh_options="-oStrictHostKeyChecking=no \
 -oLogLevel=quiet"
 
 # Parse hostfile
-unique_host_list='127.0.0.1'
-num_unique_hosts=1
+host_list=`cat $worker_file | awk '{ print $2 }'`
+unique_host_list=`cat $worker_file | awk '{ print $2 }' | uniq`
+num_unique_hosts=`cat $worker_file | awk '{ print $2 }' | uniq | wc -l`
 # User please specify device_id for multi GPUs here.
 devices="0"
 
@@ -64,55 +65,42 @@ net_outputs_prefix="${output_dir}/${dataset}"
 
 # Kill previous instances of this program
 echo "Killing previous instances of '$progname' on servers, please wait..."
-for ip in $unique_host_list; do
-  ssh $ssh_options $ip \
-    killall -q $progname
-done
+pkill -f $progname
 echo "All done!"
 
 # Spawn program instances
 client_id=0
-for ip in $unique_host_list; do
-  echo Running client $client_id on $ip
-  log_path=${log_dir}.${client_id}
+echo Running client $client_id on localhost
+log_path=${log_dir}.${client_id}
 
-  cmd="'mkdir -p ${output_dir}; \
-      mkdir -p ${log_path}; \
-      ulimit -c unlimited; \
-      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${caffe_libs}:${third_party_libs}:/usr/local/cuda/lib64; \
-      GLOG_logtostderr=false \
-      GLOG_stderrthreshold=0 \
-      GLOG_log_dir=$log_path \
-      GLOG_v=20 \
-      GLOG_minloglevel=0 \
-      GLOG_vmodule="" \
-      OPENBLAS_NUM_THREADS=${num_openblas_threads} \
-      $prog_path train \
-      --consistency_model $consistency_model \
-      --init_thread_access_table=true \
-      --hostfile $host_file \
-      --client_id ${client_id} \
-      --num_clients $num_unique_hosts \
-      --num_table_threads $num_table_threads \
-      --table_staleness $param_table_staleness \
-      --num_comm_channels_per_client $num_comm_channels_per_client \
-      --num_rows_per_table $num_rows_per_table \
-      --svb=$svb \
-      --stats_path ${output_dir}/caffe_stats.yaml \
-      --solver=${solver_filename} \
-      --net_outputs=${net_outputs_prefix} \
-      --gpu=${devices}'" #\
-      #--snapshot=${snapshot_filename}'"
+cmd="'mkdir -p ${output_dir}; \
+    mkdir -p ${log_path}; \
+    ulimit -c unlimited; \
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${caffe_libs}:${third_party_libs}:/usr/local/cuda/lib64; \
+    GLOG_logtostderr=false \
+    GLOG_stderrthreshold=0 \
+    GLOG_log_dir=$log_path \
+    GLOG_v=20 \
+    GLOG_minloglevel=0 \
+    GLOG_vmodule="" \
+    OPENBLAS_NUM_THREADS=${num_openblas_threads} \
+    $prog_path train \
+    --consistency_model $consistency_model \
+    --init_thread_access_table=true \
+    --hostfile $host_file \
+    --client_id ${client_id} \
+    --num_clients 1 \
+    --num_table_threads $num_table_threads \
+    --table_staleness $param_table_staleness \
+    --num_comm_channels_per_client $num_comm_channels_per_client \
+    --num_rows_per_table $num_rows_per_table \
+    --svb=$svb \
+    --stats_path ${output_dir}/caffe_stats.yaml \
+    --solver=${solver_filename} \
+    --net_outputs=${net_outputs_prefix} \
+    --gpu=${devices}'" #\
+    #--snapshot=${snapshot_filename}'"
 
-  ssh $ssh_options $ip bash -c $cmd &
-  #eval $cmd  # Use this to run locally (on one machine).
-
-  # Wait a few seconds for the name node (client 0) to set up
-  if [ $client_id -eq 0 ]; then
-    echo $cmd   # echo the cmd for just the first machine.
-    echo "Waiting for name node to set up..."
-    sleep 3
-    echo "Done."
-  fi
-  client_id=$(( client_id+1 ))
-done
+echo $cmd
+#ssh $ssh_options $ip bash -c $cmd &
+eval $cmd # Use this to run locally (on one machine).
