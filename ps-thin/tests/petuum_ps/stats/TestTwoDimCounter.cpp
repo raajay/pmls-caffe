a0365f1921efbfbc5ffc2467138ded8aaa4e3a85
Raajay Viswanathan <raajay.v@gmail.com>
2017-10-03 14:29:28 -0600

    Fix gtest for Two dim counter

--- ps-thin/tests/petuum_ps/stats/TestTwoDimCounter.cpp
+++ ps-thin/tests/petuum_ps/stats/TestTwoDimCounter.cpp
@@ -10,6 +10,17 @@ TEST(TestTwoDimCounter, Test1) {
   TwoDimCounter<int32_t, int32_t, int32_t> counter;
   counter.Increment(0, 1, 2);
   counter.Increment(0, 1, 2);
-  EXPECT_EQ(counter.Get(0), 4);
+  counter.Increment(0, 2, 4);
+  counter.Increment(1, 1, 3);
+  EXPECT_EQ(counter.Get(0), 8);
+  EXPECT_EQ(counter.GetAll(), 11);
+  EXPECT_EQ(counter.Get(0, 1), 4);
+  EXPECT_EQ(counter.Get(22), 0);
+  EXPECT_EQ(counter.Get(22, 23), 0);
+  EXPECT_EQ(counter.Get(1, 23), 0);
+  counter.Reset();
+  EXPECT_EQ(counter.Get(0), 0);
+  EXPECT_EQ(counter.Get(1), 0);
+  EXPECT_EQ(counter.GetAll(), 0);
 }
 }
