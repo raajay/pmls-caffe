8699dfc9fb62cb3c4f7eacec1b1d6e5da5d0face
Raajay Viswanathan <raajay.v@gmail.com>
2017-10-09 13:46:18 -0500

    nit

--- ps-thin/src/petuum_ps/namenode/name_node.cpp
+++ ps-thin/src/petuum_ps/namenode/name_node.cpp
@@ -5,7 +5,7 @@ NameNodeThread *NameNode::name_node_thread_;
 pthread_barrier_t NameNode::init_barrier_;
 
 void NameNode::Init() {
-  pthread_barrier_init(&init_barrier_, NULL, 2);
+  pthread_barrier_init(&init_barrier_, nullptr, 2);
   name_node_thread_ = new NameNodeThread(&init_barrier_);
   name_node_thread_->Start();
   pthread_barrier_wait(&init_barrier_);
