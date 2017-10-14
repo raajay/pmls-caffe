630f079a3968dc1f9da9ea5238d1aa153b520a04
Raajay Viswanathan <raajay.v@gmail.com>
2017-10-14 01:50:22 -0600

    Add delay tracking at the scheduler

--- ps-thin/src/petuum_ps/scheduler/pass_through_scheduler.cpp
+++ ps-thin/src/petuum_ps/scheduler/pass_through_scheduler.cpp
@@ -1,4 +1,5 @@
 #include <petuum_ps/scheduler/pass_through_scheduler.hpp>
+#include <petuum_ps/thread/context.hpp>
 
 namespace petuum {
 
@@ -10,25 +11,47 @@ namespace petuum {
         delete internal_queue_;
     }
 
-    void PassThroughScheduler::AddRequest(MLFabricRequest *request) {
+    /**
+     */
+    void PassThroughScheduler::AddRequest(MLFabricRequest *request) {/*{{{*/
         if (request != nullptr) {
           request->OutputDestinationId = request->InputDestinationId;
           VLOG(20) << "Add : " << request->toString();
         }
         internal_queue_->Push(request);
-    }
+    }/*}}}*/
 
+    /**
+     */
     MLFabricRequest* PassThroughScheduler::TakeRequest() {
         MLFabricRequest* value = nullptr;
         bool success = false;
         while (!success) {
           success = internal_queue_->Pop(&value);
           if (success) break;
-
           // see: https://stackoverflow.com/questions/4184468/sleep-for-milliseconds
           std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+
+        // XXX(raajay): this is a hack to keep track of the number of oplogs
+        // that are forwarded to the server. It may so happen that the
+        // scheduler send threads may not forward the requests to the source
+        // thread.
+        if (nullptr != value) {
+            UpdateOplogCounter(value);
         }
         return value;
     }
 
+    /**
+     */
+    void PassThroughScheduler::UpdateOplogCounter(MLFabricRequest *request) {
+        int32_t dest_id = request->OutputDestinationId;
+        if (!GlobalContext::is_server_thread(dest_id)) {
+            return;
+        }
+        int32_t delay = oplog_counter_.Get(dest_id) - request->OplogVersion;
+        VLOG(0) << "Delay : " << delay;
+        oplog_counter_.Increment(dest_id, 1);
+    }
 }
